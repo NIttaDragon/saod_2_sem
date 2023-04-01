0b832dc957d3ab427a9250783944f1c01e1267cf
NIttaDragon <101250164+NIttaDragon@users.noreply.github.com>
2023-04-01 16:32:45 +0300

    Update saod7.cpp

--- saod7.cpp
+++ saod7.cpp
@@ -16,18 +16,10 @@ int main()
   for(int i = 0; i<b; i++) //заполнение матрицы координат
   {
     cout<<"Введите координаты для вектора "<<i+1<<": \n";
-    cout<<"x1=";
-    cin>>g;
-    otr[i][0] = g;
-    cout<<"y1=";
-    cin>>g;
-    otr[i][1] = g;
-    cout<<"x2=";
-    cin>>g;
-    otr[i][2] = g;
-    cout<<"y1=";
-    cin>>g;
-    otr[i][3] = g;
+    cout<<"x1=";  cin>>g;  otr[i][0] = g;
+    cout<<"y1=";  cin>>g;  otr[i][1] = g;
+    cout<<"x2=";  cin>>g;  otr[i][2] = g;
+    cout<<"y1=";  cin>>g;  otr[i][3] = g;
   }
 
   for(int i = 0; i<b; i++) //вывод матрицы
@@ -48,10 +40,8 @@ int main()
       otr[i][1] = otr[i][3];
       otr[i][3] = e;
     }
-    if(otr[i][1]==otr[i][3])
-      a[i]=0;
-    else
-      a[i]=(otr[i][3]-otr[i][1])/(otr[i][2]-otr[i][0]);
+    if(otr[i][1]==otr[i][3])  a[i]=0;
+    else  a[i]=(otr[i][3]-otr[i][1])/(otr[i][2]-otr[i][0]);
   }
 
   for(int i = 0; i<b; i++) //вывод матрицы
@@ -64,14 +54,12 @@ int main()
   for(int i = 0; i<b-1; i++)
     for(int j =i+1; j<b;j++)
     {
-      if(a[i]==a[j])
-        cout<<"Отрезки"<<i<<"и"<<j<<"параллельны \n";
+      if(a[i]==a[j])  cout<<"Отрезки"<<i<<"и"<<j<<"параллельны \n";
       else
       {
         if((otr[i][0]<=otr[j][2] and otr[j][2]<=otr[i][2])or(otr[i][0]<=otr[j][1] and otr[j][1]<=otr[i][2]))
           cout<<"Отрезки "<<i+1<<" и "<<j+1<<" пересекаются \n";
-        else
-          cout<<"Отрезки не пересекаются\n";
+        else  cout<<"Отрезки не пересекаются\n";
       }
     }
 
